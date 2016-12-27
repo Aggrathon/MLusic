8b71802cca3974a7ca9cfc4401f35c9a0bd6861f
Aggrathon <antonbjo@gmail.com>
2016-12-27 17:06:05 +0200

    Parse Notes

--- input_analyzer.py
+++ input_analyzer.py
@@ -14,16 +14,30 @@ if __name__ == "__main__":
     plt.hist([s.beat_unit for s in songs], bins=range(2,13))
     plt.title("Beat Lengths")
     plt.show()
+
     plt.hist([s.tempo for s in songs], bins="auto")
     plt.title("Tempo")
     plt.show()
     plt.hist([s.ticks_per_quarter for s in songs], bins="auto")
     plt.title("ticks_per_quarter")
     plt.show()
-    plt.hist([len([i for i in s.instruments if i != -1]) for s in songs], bins=range(1,16))
+
+    plt.hist([len(s.instruments) for s in songs], bins=range(1,16))
     plt.title("Number of Instruments")
     plt.show()
-    plt.hist(list(np.array([[i for i in s.instruments if i != -1] for s in songs]).flat), bins="auto")
+    plt.hist(list(np.array([s.instruments for s in songs]).flat), bins="auto")
     plt.title("Instruments")
+    plt.show()
+    plt.hist(list(np.array([len(s.tracks) for s in songs]).flat), bins="auto")
+    plt.title("Tracks")
+    plt.show()
+
+    notes = []
+    for s in songs:
+        for t in s.tracks:
+            for n in t:
+                notes.append(n[1])
+    plt.hist(notes, bins=range(35,86))
+    plt.title("Notes")
     plt.show()
 
--- song.py
+++ song.py
@@ -11,7 +11,9 @@ class Song(object):
     tempo = 0
     ticks_per_quarter = 0
     instruments = []
+    track_instrument = []
     tracks = []
+
 
     def __init__(self, file_name):
         file = open(file_name, "r")
@@ -19,31 +21,69 @@ class Song(object):
         self.ticks_per_quarter = int(line[line.rfind(",")+2:])
         self.name = file_name[file_name.find("/")+1:file_name.rfind(".")]
         self.instruments = []
+        self.track_instrument = []
+        track_key = dict()
+        instrument_key = dict()
+        self.tracks = []
 
         line = file.readline()
         while line != "":
             split = line.split(", ")
-            if split[2] == "Note_on_c":
-                pass
-            elif split[2] == "Note_off_c":
-                pass
+
+            if split[2] == "Note_on_c": #creating a note with zero length
+                track = int(split[0])
+                time = int(split[1])
+                instrument = instrument_key[int(split[3])]
+                note = int(split[4])
+                velocity = int(split[5])
+                key = track*256+instrument
+                try:
+                    track = track_key[key]
+                except (KeyError, TypeError):
+                    track = len(self.tracks)
+                    self.tracks.append([])
+                    track_key[key] = track
+                    self.track_instrument.append(instrument)
+                self.tracks[track].append([time, note, velocity, 0])
+
+            elif split[2] == "Note_off_c": #setting the length of a note
+                track = int(split[0])
+                time = int(split[1])
+                instrument = instrument_key[int(split[3])]
+                note = int(split[4])
+                key = track*256+instrument
+                track = track_key[key]
+                index = len(self.tracks[track])-1
+                while self.tracks[track][index][1] != note:
+                    index -= 1
+                    if index < 0:
+                        print(self.tracks[track])
+                self.tracks[track][index][-1] = time - self.tracks[track][index][0]
+
             elif split[2] == "Tempo":
                 self.tempo = int(split[3])
+
             elif split[2] == "Time_signature":
                 self.bar_length = int(split[3])
                 self.beat_unit = 2**int(split[4])
+
             elif split[2] == "Program_c":
                 channel = int(split[3])
-                while len(self.instruments) <= channel:
-                    self.instruments.append(-1)
-                self.instruments[channel] = int(split[4])
-            elif split[2] == "Key_signature":
-                pass
+                instrument = int(split[4])
+                try:
+                    index = self.instruments.index(instrument)
+                    instrument_key[channel] = index
+                except ValueError:
+                    instrument_key[channel] = len(self.instruments)
+                    self.instruments.append(instrument)
+
             line = file.readline()
         file.close()
 
+
     def __repr__(self):
         return "<Song '{}'>".format(self.name)
+
 
     def save_to_file(self, file_name=None):
         if file_name is None:
@@ -51,6 +91,7 @@ class Song(object):
         file = open(file_name, "w")
         #generate csv
         file.close()
+
 
 def read_all_inputs():
     return [Song(INPUT_FOLDER+s) for s in os.listdir(INPUT_FOLDER) if s.endswith(".csv")]
