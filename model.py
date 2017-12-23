3e3aaecc37e4ee2d5514b4cd7a128fbb8b4d8595
Aggrathon <antonbjo@gmail.com>
2017-12-23 12:08:13 +0200

    Add batch normalization

--- model.py
+++ model.py
@@ -40,30 +40,41 @@ def model_fn(features, labels, mode, params=dict()):
     audio = features['input']
     batch_size = params.get('batch_size', BATCH_SIZE)
     learning_rate = params.get('learning_rate', LEARNING_RATE)
+    training = mode == tf.estimator.ModeKeys.TRAIN
     with tf.variable_scope("generator"):
         prev_layer = tf.random_uniform((batch_size, 100), 0, 1, tf.float32)
         prev_layer = tf.layers.dense(prev_layer, 1000, tf.nn.relu)
         prev_layer = tf.layers.dense(prev_layer, 2000, tf.nn.relu)
         prev_layer = tf.reshape(prev_layer, (batch_size, 20, 1, 100))
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d_transpose(prev_layer, 80, (33, 1), (16, 1), activation=tf.nn.relu, name='conv_transpose_0')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d_transpose(prev_layer, 64, (17, 1), (10, 1), activation=tf.nn.relu, name='conv_transpose_1')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d_transpose(prev_layer, 48, (13, 1), (8, 1), activation=tf.nn.relu, name='conv_transpose_2')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d_transpose(prev_layer, 32, (9, 1), (6, 1), activation=tf.nn.relu, name='conv_transpose_3')
-        prev_layer = tf.layers.conv2d_transpose(prev_layer, 1, (7, 1), (4, 1), activation=tf.nn.relu, name='conv_transpose_4') # MAYBE tanh
-        prev_layer = tf.reshape(prev_layer, prev_layer.get_shape().as_list()[:3])
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
+        prev_layer = tf.layers.conv2d_transpose(prev_layer, 1, (7, 1), (4, 1), activation=tf.nn.tanh, name='conv_transpose_4')
+        output = tf.reshape(prev_layer, prev_layer.get_shape().as_list()[:3])
 
     if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=dict(output=prev_layer))
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=dict(output=output))
     
-    prev_layer = tf.concat((prev_layer, audio), 0)
-    labels = tf.convert_to_tensor([[0.05]]*batch_size + [[0.95]]*batch_size)
+    prev_layer = tf.concat((output, audio), 0)
+    labels = tf.convert_to_tensor([[0.1]]*batch_size + [[0.9]]*batch_size)
     with tf.variable_scope('discriminator'):
         prev_layer = tf.reshape(prev_layer, prev_layer.get_shape().as_list()+[1])
         prev_layer = tf.layers.conv2d(prev_layer, 40, (9, 1), (6, 1), activation=tf.nn.relu, name='conv_0')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d(prev_layer, 48, (9, 1), (6, 1), activation=tf.nn.relu, name='conv_1')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d(prev_layer, 56, (9, 1), (6, 1), activation=tf.nn.relu, name='conv_2')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d(prev_layer, 64, (13, 1), (8, 1), activation=tf.nn.relu, name='conv_3')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.layers.conv2d(prev_layer, 72, (15, 1), (10, 1), activation=tf.nn.relu, name='conv_4')
+        prev_layer = tf.layers.batch_normalization(prev_layer, training=training)
         prev_layer = tf.contrib.layers.flatten(prev_layer)
         prev_layer = tf.layers.dense(prev_layer, 1024, activation=tf.nn.relu, name='fc_0')
         prev_layer = tf.layers.dense(prev_layer, 256, activation=tf.nn.relu, name='fc_1')
@@ -74,9 +85,14 @@ def model_fn(features, labels, mode, params=dict()):
     with tf.variable_scope('training'):
         loss_disc = tf.losses.sigmoid_cross_entropy(labels, logits)
         loss_gen = tf.losses.sigmoid_cross_entropy(1.0-labels, logits)
+        loss_sim = tf.abs(0.5 - tf.abs(output-tf.reduce_sum(output, 1)/tf.size(output)[0]))
+        tf.summary.scalar("DiscriminatorLoss", loss_disc)
+        tf.summary.scalar("GeneratorLoss", loss_gen)
+        tf.summary.scalar("SimilarityLoss", loss_sim)
         adam = tf.train.AdamOptimizer(learning_rate)
-        trainer_disc = adam.minimize(loss_disc, global_step=tf.contrib.framework.get_global_step(), var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "discriminator"))
-        trainer_gen = adam.minimize(loss_gen, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "generator"))
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            trainer_disc = adam.minimize(loss_disc, global_step=tf.contrib.framework.get_global_step(), var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "discriminator"))
+            trainer_gen = adam.minimize(loss_gen*(1.0-loss_disc)**2+loss_sim*0.05, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "generator"))
 
         return tf.estimator.EstimatorSpec(
             mode=mode,
